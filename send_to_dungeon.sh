a0f11683899e3dfa51a348dbbb76aa81a1d9eee5
root <root@dnd-core.localdomain>
2017-02-12 01:47:03 +0000

    Updating send_to_dungeon for private IP

--- send_to_dungeon.sh
+++ send_to_dungeon.sh
@@ -4,9 +4,9 @@ DB="dnd_database"
 
 DIR="/tmp/prisoners"
 mkdir -p $DIR
-FILE="prisoner_"`date "+%Y%m%d%H%M%S"`
+FILE="prisoner_"`date "+%Y-%m-%d_%H:%M:%S"`
 
-TARGET="root@138.68.62.110"
+TARGET="root@10.138.112.90"
 TARGET_DIR="~/prisoners"
 
 dokku postgresql:export $DB | gzip > $DIR/$FILE
